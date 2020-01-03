fc3d07e41e31ced313d547e5a01f5355a27f7741
Marco Franceschetti <vonfritz1@gmail.com>
2020-01-03 19:42:44 +0100

    update MYSQL db script

--- MySQL_Update_7.sql
+++ MySQL_Update_7.sql
@@ -7,12 +7,15 @@ INSERT IGNORE INTO `Settings` (`Section`, `Parameter`, `value`, `Description_DE`
 ('GENERAL', 'SHOWSUMMARY', '1', 'Anzeige von Device in der Übersicht auf der Hauptseite. (0: nein 1: ja)', 'Show device in summary on main page (0: no 1: yes)', 'Visualizzazione del dispositivo nella panoramica sulla pagina principale. (0: no 1: sì)', '_DEFAULT');
 
 REPLACE INTO `Strings` (`File`, `Field`, `Description_DE`, `Description_EN`, `Description_IT`) VALUES
-('index', 'adjust_history', 'Historie anpassen', 'Adjust history', 'Aggiorna storia'),
+('index', 'change_history', 'Historie anpassen', 'Adjust history', 'Aggiorna storia'),
+('index', 'help', 'Hilfe', 'Help', 'Aiuto'),
+('index', 'no_data', 'Keine Daten in den letzten %1$d Tagen gespeichert. Bitte Spindel Verbinden, damit Daten angezeigt werden können.<br /><br />Oder ändern sie die Anzahl der Tage:<br />\r\n\r\n', 'No spindle data received in the past %1$d days. Please connect Spindle to generate data.<br /><br />Or change days of history:<br />', 'Nessun dato salvato negli ultimi %1$d giorni. Collegare l\'ispindle in modo che dati recenti possano essere visualizzati.<br /> <br /> Oppure modifica il numero di giorni: <br />'),
 ('index', 'header_initialgravity', 'Stammwürze [°P]', 'Initial gravity [°P]', 'Densità iniziale OG [°P]'),
-('index', 'no_data', 'Keine Daten in den letzten %1$d Tagen gespeichert. Bitte Spindel Verbinden, damit Daten angezeigt werden können.', 'No spindle data received in the past %1$d days. Please connect Spindle to generate data.', 'Nessun dato salvato negli ultimi %1$d giorni. Collegare l\'ispindle che i dati possano essere visualizzati.'),
 ('index', 'server_running', 'TCP Server läuft mit PID: ', 'TCP Server is running with PID: ', 'Server TCP avviato con PID: '),
 ('sendmail', 'subject_alarm_low_gravity', 'Alarm von iSpindel-TCP-Server (%s): Gravity unter Limit gefallen', 'Alarm from iSpindel-TCP-Server (%s): Gravity below limit', 'Allarme dal server-TCP-iSpindle (%s): Densità inferiore al limite'),
 ('sendmail', 'subject_alarm_svg', 'Alarm von iSpindel-TCP-Server (%s): Vergärungsgrad oberhalb Alarm Limit', 'Alarm from iSpindel-TCP-Server (%s): Apparent attenuation above alarm limit', 'Allarme dal server-TCP-iSpindle (%s): Attenuazione apparente superiore al limite'),
 ('sendmail', 'subject_status', 'Status Email von iSpindel-TCP-Server (%s)', 'Status Email from iSpindel-TCP-Server (%s)', 'Email di stato dal server-TCP-iSpindle (%s)'),
 ('settings', 'add_device', 'Individuelle Settings für Device anlegen', 'Add individual settings for device', 'Crea impostazioni individuali per il dispositivo'),
-('settings', 'delete_device', 'Device aus individuellen Settings löschen', 'Remove Device from individual Settings', 'Elimina il dispositivo dalle singole impostazioni');
+('settings', 'delete_device', 'Device aus individuellen Settings löschen', 'Remove Device from individual Settings', 'Elimina il dispositivo dalle singole impostazioni'),
+('settings', 'export_data', 'Daten Tabelle exportieren', 'Export Data Table', 'Esporta tabella dati'),
+('settings', 'testmail', 'Sende Test Email', 'Send test email', 'Invia e-mail di prova');
